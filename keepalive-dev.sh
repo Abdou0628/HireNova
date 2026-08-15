8156161e1e28220ce8e8555f7b5e4a1b378e3223
Z User <z@container>
2026-08-15 15:54:41 +0000

    7d2aabcb-2ee7-4e63-97c8-1d8fb4c0f4c9

--- keepalive-dev.sh
+++ keepalive-dev.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-cd /home/z/my-project
 while true; do
-  bun run dev >> /home/z/my-project/dev.log 2>&1
-  echo "[keepalive] Server died, restarting in 2s..." >> /home/z/my-project/dev.log
+  echo "[$(date '+%H:%M:%S')] Starting dev server..."
+  bun run dev > dev.log 2>&1
+  EXIT_CODE=$?
+  echo "[$(date '+%H:%M:%S')] Dev server exited with code $EXIT_CODE, restarting in 2s..."
   sleep 2
 done
